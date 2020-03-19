f137d2079d463c9c987df1d33019354d34aa36df
Timothy <35541279+timhow38@users.noreply.github.com>
2020-03-20 05:42:42 +1300

    Update

--- Part 01/02 - Layouts, Constraint Layout/Act9 - Done/Proj09-B/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Part 01/02 - Layouts, Constraint Layout/Act9 - Done/Proj09-B/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,41 +1,44 @@
 package com.example.myapplication;
 
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
 
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import android.transition.ChangeBounds;
 import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
- 
+
 public class MainActivity extends AppCompatActivity {
     private ConstraintLayout layout;
     private ConstraintSet constraintSetOld = new ConstraintSet();
     private ConstraintSet constraintSetNew = new ConstraintSet();
     private boolean altLayout;
- 
- 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
- 
+
         layout = findViewById(R.id.layout);
- 
+
         constraintSetOld.clone(layout);
         constraintSetNew.clone(this, R.layout.activity_main_alt);
     }
- 
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void swapView(View v) {
         Transition changeBounds = new ChangeBounds();
         changeBounds.setInterpolator(new OvershootInterpolator());
- 
+
         TransitionManager.beginDelayedTransition(layout, changeBounds);
- 
+
         if (!altLayout) {
             constraintSetNew.applyTo(layout);
             altLayout = true;
